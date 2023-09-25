38bedb303b94df6766f2089e5d52257fae1fa8d3
joy.hwan <joy.hwan@kakaocorp.com>
2023-09-25 15:12:16 +0900

    feat : 하단 및 헤더 버튼 추가 및 수정

--- src/pages/home/components/HomeButton.tsx
+++ src/pages/home/components/HomeButton.tsx
@@ -1,71 +1,37 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 function HomeButton() {
-  return (
-    <Con>
-    <Wrapper>
-      <Div 
-        width="140px"
-        height="140px"
-        marginTop="40px"
-        marginRight="-40px"
-      >
-        <P fontSize="28px">일기 쓰기</P>
-      </Div>
-      <Div><P>종료</P></Div>
-      <Div><P>일기 목록</P></Div>
-      <Div
-        marginTop="-60px"
-        marginRight="60px"
-      >
-        <P>행운 기록</P>
-      </Div>
-    </Wrapper>
-    </Con>
-  )
+	return (
+		<Wrapper>
+			<Div>오늘 일기를 아직 작성하지 않으셨어요 !</Div>
+		</Wrapper>
+	);
 }
 
 export default HomeButton;
 
-const Con = styled.div`
-  width: 100%;
-  height: 35%;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
 const Wrapper = styled.div`
+	margin-top: 10%;
 
-  display: grid;
-  grid-template-rows: repeat(2, 140px);
-  grid-template-columns: repeat(2, 140px);
-  place-items: center;
+	height: 12%;
+	width: 100%;
 
-  gap: 7%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
-  transform: rotate( 45deg );
-`
+const Div = styled.div`
+	height: 100%;
+	width: 90%;
 
-const Div = styled.div<{width?: string, height?: string, margin?: string, marginTop?: string, marginRight?: string}>`
-  background-color: yellow;
-  width: ${props => props.width || '80px'};
-  height: ${props => props.height || '80px'};
-  border-radius: 50%;
-  margin: ${props => props.margin || '1%'};
+	background-color: #f9f3cc;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-  margin-top: ${props => props.marginTop};
-  margin-right: ${props => props.marginRight};
-  background-color: #FFE3E1;
-`
+	font-size: 18px;
 
-const P = styled.p<{fontSize?: string}>`
-  transform: rotate( -45deg );
-  font-weight: bold;
-  font-size: ${props => props.fontSize || '16px'};
-`
+	border-radius: 3%;
+`;
