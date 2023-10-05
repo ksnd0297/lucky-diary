55a555183ca7cd502050a3ed6b8bf6eafe6dac5f
joy.hwan <joy.hwan@kakaocorp.com>
2023-10-06 02:36:33 +0900

    feat : 일기 작성 컴포넌트 제작

--- src/pages/fill/components/Header.tsx
+++ src/pages/fill/components/Header.tsx
@@ -1,11 +1,34 @@
 import Image from 'next/image';
 import styled from 'styled-components';
+import { Dispatch, SetStateAction } from 'react';
+import useModal from '@/hooks/useModal';
 
-function Header() {
+type HeaderProps = {
+	text: string;
+	setText: Dispatch<SetStateAction<string>>;
+};
+
+function Header(props: HeaderProps) {
+	const { text, setText } = props;
+
+	const { hideModal } = useModal();
+
+	const onClose = () => {
+		hideModal();
+		setText('');
+	};
+
+	const onSubmit = () => {
+		// TODO : 웹뷰로 텍스트 데이터 전송
+		console.log('text : ', text);
+
+		onClose();
+	};
+
 	return (
 		<Wrapper>
-			<Image src="svg/close-svgrepo-com.svg" alt="일기 작성" width={30} height={30} />
-			<Image src="svg/pencil-svgrepo-com.svg" alt="일기 작성" width={40} height={40} />
+			<Image src="svg/close-svgrepo-com.svg" alt="일기 작성" width={25} height={25} onClick={onClose} />
+			<Image src="svg/pencil-svgrepo-com.svg" alt="일기 작성" width={40} height={40} onClick={onSubmit} />
 		</Wrapper>
 	);
 }
@@ -14,11 +37,14 @@ export default Header;
 
 const Wrapper = styled.div`
 	width: 100%;
-	height: 5%;
+	height: 6%;
+
+	position: sticky;
 
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
 
 	padding: 2%;
+	background: #fafafa;
 `;
--- src/pages/fill/index.tsx
+++ src/pages/fill/index.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import Modal from '../../components/Modal';
 import Header from './components/Header';
 import Write from './components/Write';
 
 function Fill() {
+	const [text, setText] = useState<string>('');
+
 	return (
 		<Modal>
 			<Wrapper>
-				<Header />
-				<Write />
+				<Header text={text} setText={setText} />
+				<Write text={text} setText={setText} />
 			</Wrapper>
 		</Modal>
 	);
