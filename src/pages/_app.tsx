96d74fe252dcc80e0e5641ae75d42c4150c8d283
ksnd0297 <45099438+ksnd0297@users.noreply.github.com>
2023-10-07 20:59:16 +0900

    fix : 메타 추가 2 (#25)
    Co-authored-by: joy.hwan <joy.hwan@kakaocorp.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,10 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
+				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
