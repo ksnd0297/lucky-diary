65829e80664ba514d313c194d9f18260f0e59b0a
joy.hwan <joy.hwan@kakaocorp.com>
2023-10-12 01:58:51 +0900

    feat : popup 컴포넌트 제작 및 로직 변경

---
+++ src/components/Popup.tsx
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+import { MouseEvent, useRef } from 'react';
+import { PopupConfigType } from '@/Constant/layout';
+import usePopup from '@/hooks/usePopup';
+import Portal from './Portal';
+
+function Popup(props: PopupConfigType) {
+	const { isOpen, children, onConfirm, onCancel } = props;
+
+	const { hidePopup } = usePopup();
+
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	const onDimmedClick = (event: MouseEvent<HTMLDivElement>) => {
+		if ((event.target as HTMLDivElement).contains(ref.current)) {
+			hidePopup();
+		}
+	};
+
+	return (
+		<Portal selector="#popup">
+			<Container>
+				{isOpen && (
+					<div className="popup" onClick={onDimmedClick} aria-hidden>
+						<Wrapper ref={ref}>
+							<ContentWrapper>
+								<Content>{children}</Content>
+							</ContentWrapper>
+							<ButtonWrapper>
+								<Button className="cancel" onClick={onCancel}>
+									취소
+								</Button>
+								<Button className="confirm" onClick={onConfirm}>
+									확인
+								</Button>
+							</ButtonWrapper>
+						</Wrapper>
+					</div>
+				)}
+			</Container>
+		</Portal>
+	);
+}
+
+export default Popup;
+
+const Container = styled.div`
+	.popup {
+		background: rgba(0, 0, 0, 0.1);
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		top: 0;
+		position: fixed;
+		left: 0;
+		text-align: left;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+`;
+
+const Wrapper = styled.div`
+	width: 60%;
+	height: 25%;
+`;
+
+const ContentWrapper = styled.div`
+	width: 100%;
+	height: 70%;
+	background-color: white;
+	border-top-left-radius: 2%;
+	border-top-right-radius: 2%;
+
+	padding: 7%;
+`;
+
+const Content = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: white;
+
+	border-radius: 2%;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	flex-direction: column;
+`;
+
+const ButtonWrapper = styled.div`
+	width: 100%;
+	height: 30%;
+	background-color: white;
+
+	border-end-start-radius: 2%;
+	border-end-end-radius: 2%;
+
+	display: flex;
+
+	justify-content: space-evenly;
+
+	.cancel {
+		background-color: #b9b9b9;
+	}
+
+	.confirm {
+		background-color: #fac85a;
+	}
+`;
+
+const Button = styled.button`
+	border: 0;
+	background-color: transparent;
+
+	width: 40%;
+	height: 70%;
+
+	border-radius: 3%;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
