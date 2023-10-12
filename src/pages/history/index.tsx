f40e2a6985ca1ef49f77dcbde32ed2949fa162dd
joy.hwan <joy.hwan@kakaocorp.com>
2023-10-13 00:59:20 +0900

    feat : History 컴포넌트 제작

---
+++ src/pages/history/index.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { subDays } from 'date-fns';
+import { Fragment } from 'react';
+import Footer from '@/components/layout/Footer';
+import List from './components/List';
+
+const list = [
+	{ date: new Date(), value: 70 },
+	{ date: subDays(new Date(), 1), value: -50 },
+	{ date: subDays(new Date(), 2), value: 30 },
+	{ date: subDays(new Date(), 3), value: 60 },
+	{ date: subDays(new Date(), 4), value: 100 },
+	{ date: subDays(new Date(), 5), value: 200 },
+	{ date: subDays(new Date(), 6), value: -120 },
+	{ date: subDays(new Date(), 7), value: -150 },
+	{ date: subDays(new Date(), 8), value: -180 },
+	{ date: subDays(new Date(), 9), value: 300 },
+];
+
+function History() {
+	return (
+		<>
+			<Wrapper>
+				{list.map((value) => (
+					<List key={value.date.getTime()} {...value} />
+				))}
+			</Wrapper>
+			<Footer />
+		</>
+	);
+}
+
+export default History;
+
+const Wrapper = styled.div`
+	width: 100%;
+	height: 92%;
+	overflow: auto;
+`;
