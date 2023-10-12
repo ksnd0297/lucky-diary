e48275169b6d6e699af025a44503d6125b6a0795
joy.hwan <joy.hwan@kakaocorp.com>
2023-10-13 01:58:28 +0900

    feat : 스타일 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { RecoilRoot, useRecoilValue } from 'recoil';
 import Modal from '@/components/Modal';
 import { modalConfigState, popupConfigState } from '@/Constant/layout';
 import Popup from '@/components/Popup';
+import Layout from '@/components/layout/Layout';
 
 function ModalComponent() {
 	const modalConfig = useRecoilValue(modalConfigState);
@@ -30,7 +31,9 @@ export default function App({ Component, pageProps }: AppProps) {
 						content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
 					/>
 				</Head>
-				<Component {...pageProps} />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
 				<ModalComponent />
 				<PopupComponent />
 			</RecoilRoot>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Layout from '../components/layout/Layout';
 import Home from './home';
 
 export default function Index() {
-	return (
-		<Layout>
-			<Home />
-		</Layout>
-	);
+	return <Home />;
 }
