3a115698b3aae0f6dda02cd1c72150100e1297e5
ksnd0297 <45099438+ksnd0297@users.noreply.github.com>
2023-10-13 01:41:46 +0900

    feat : history 컴포넌트 제작 및 팝업 탑재 (#32)
    * feat : 팝업 인자 추가
    
    * feat : 컴포넌트 라우팅 및 스타일 수정
    
    * feat : History 컴포넌트 제작
    
    * fix : 날짜 로직 변경
    
    * fix : lint 오류 수정
    
    ---------
    
    Co-authored-by: joy.hwan <joy.hwan@kakaocorp.com>

---
+++ src/pages/history/components/List.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import useModal from '@/hooks/useModal';
+import Fill from '@/pages/fill';
+import { getFormatDate } from '@/utils/date';
+
+function List({ date, value }: { date: Date; value: number }) {
+	const { showModal } = useModal();
+
+	const onClick = (wroteText: string) => {
+		showModal({
+			children: <Fill wroteText={wroteText} isWrote />,
+		});
+	};
+
+	return (
+		<Wrapper onClick={() => onClick(date.toString())}>
+			<Div fontSize="18px">{getFormatDate(date)}</Div>
+			<Div fontSize="16px" flex center>
+				{value >= 0 ? '❤️' : '💙'}&nbsp;&nbsp;
+				<Div color={value >= 0 ? 'red' : 'blue'}>{value >= 0 ? `+${value}` : value}</Div>
+			</Div>
+			<Div fontSize="14px" flex center>
+				<Div color={value >= 0 ? 'red' : 'blue'}>{value}</Div>&nbsp;의 행운코인을&nbsp;
+				<Div color={value >= 0 ? 'red' : 'blue'}>{value >= 0 ? '획득' : '소비'}</Div>
+				&nbsp;했어요
+			</Div>
+			<Line />
+		</Wrapper>
+	);
+}
+
+export default List;
+
+const Wrapper = styled.div`
+	width: 100%;
+	height: 15%;
+
+	display: flex;
+
+	align-items: center;
+	justify-content: space-evenly;
+
+	flex-direction: column;
+
+	margin-top: 5%;
+	margin-bottom: 5%;
+`;
+
+const Div = styled.div<{ fontSize?: string; color?: string; flex?: boolean; center?: boolean }>`
+	font-size: ${(props) => props.fontSize};
+	color: ${(props) => props.color};
+	display: ${(props) => props.flex && 'flex'};
+	align-items: ${(props) => props.center && 'center'};
+	justify-content: ${(props) => props.center && 'center'};
+`;
+
+const Line = styled.div`
+	width: 10%;
+	border: 1px solid #c7c7c7;
+`;
