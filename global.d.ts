0556d67ae82128ec50d3d1ff0fc69d7dc6894955
joy.hwan <joy.hwan@kakaocorp.com>
2023-11-12 20:01:43 +0900

    feat : 안드로이드 doucmnet 세팅

--- global.d.ts
+++ global.d.ts
@@ -13,6 +13,23 @@ interface Window {
 	};
 }
 
+interface Document {
+	webkit: Webkit;
+	/**
+	 * A convenience API that we seem to expose in iOS.
+	 * Not sure whether Android does the same.
+	 * @see: https://github.com/react-native-community/react-native-webview/blob/25552977852427cf5fdc7b233fd1bbc7c77c18b0/ios/RNCWebView.m#L1128-L1146
+	 */
+	ReactNativeWebView: {
+		postMessage(message: string): void;
+	};
+
+	addEventListener<K extends keyof WindowEventMap>(type: K, listener: (this: Window, ev: WindowEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener<K extends keyof WindowEventMap>(type: K, listener: (this: Window, ev: WindowEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+}
+
 interface Webkit {
 	messageHandlers: {
 		/**
